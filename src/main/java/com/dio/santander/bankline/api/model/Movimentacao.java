fd97f4f38b494e39694836fae27cdfc8f82d460f
dyeggoSilva <83801890+dyeggoSilva@users.noreply.github.com>
2022-06-05 20:18:36 -0300

    versão final do  projeto

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -19,15 +19,22 @@ public class Movimentacao {
 	private Integer id;
 	@Column(name="data_hora")
 	private LocalDateTime dataHora;
-	@Column(name="conta_numero")
+	@Column(name="descricao")
 	private String descricao;
 	private Double valor;
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
-	
+	@Column(name="id_conta")
+	private Integer idConta;
 	
 	//Início Gat and Set
 	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 	public Integer getId() {
 		return id;
 	}
